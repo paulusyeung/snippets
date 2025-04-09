3a71189d076a26c8ab3fc9e7952d8c088df87485
paulusyeung <paulusyeung@gmail.com>
2025-04-09 23:03:19 +0800

    Update ubuntu-install-portainer.sh
    Add update steps

--- docker/ubuntu-install-portainer.sh
+++ docker/ubuntu-install-portainer.sh
@@ -1,3 +1,21 @@
+-------------------------------------
+# install a new portainer
+
 docker volume create portainer_data
 
+docker run -d -p 8000:8000 -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+
+-------------------------------------
+# update a live portainer
+
+# download the latest portainer image
+docker image pull portainer/portainer-ce:latest
+
+# stop the portainer container
+docker container stop portainer_name
+
+# remove the portainer container
+docker container rm portainer_name
+
+# create the portainer container
 docker run -d -p 8000:8000 -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
